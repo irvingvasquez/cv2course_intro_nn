4a34d129cd059f26ac1dc755bb5a67a8477d204e
J. Irving Vasquez-Gomez <vakeome@gmail.com>
2017-08-16 11:02:02 -0500

    and modified

--- perceptron_and.py
+++ perceptron_and.py
@@ -1,25 +1,27 @@
+# Based on the Udacity SDC course
+
 import pandas as pd
 
-# TODO: Set weight1, weight2, and bias
-weight1 = 1.0
-weight2 = 1.0
-bias = -2.0
+# TODO: Establecer weight1, weight2, y bias
+weight1 = 0.0
+weight2 = 0.0
+bias = -0.0
 
 
-# DON'T CHANGE ANYTHING BELOW
-# Inputs and outputs
+# ESTO DEBE QUEDAR IGUAL
+# Inputs, outputs
 test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
 correct_outputs = [False, False, False, True]
 outputs = []
 
-# Generate and check output
+# Generar y revisar
 for test_input, correct_output in zip(test_inputs, correct_outputs):
     linear_combination = weight1 * test_input[0] + weight2 * test_input[1] + bias
     output = int(linear_combination >= 0)
     is_correct_string = 'Yes' if output == correct_output else 'No'
     outputs.append([test_input[0], test_input[1], linear_combination, output, is_correct_string])
 
-# Print output
+# Escribir resultados
 num_wrong = len([output[4] for output in outputs if output[4] == 'No'])
 output_frame = pd.DataFrame(outputs, columns=['Input 1', '  Input 2', '  Linear Combination', '  Activation Output', '  Is Correct'])
 if not num_wrong:
