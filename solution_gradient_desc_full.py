1f48c11fd2294a19d59fd271aa5479a77333d57d
J. Irving Vasquez-Gomez <vakeome@gmail.com>
2017-08-28 15:26:45 -0500

    gradient descent full excesice

---
+++ solution_gradient_desc_full.py
@@ -0,0 +1,62 @@
+import numpy as np
+from data_prep import features, targets, features_test, targets_test
+
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
+    
+def sigmoid_prime(x):
+    return sigmoid(x) * (1-sigmoid(x))
+
+# Use to same seed to make debugging easier
+np.random.seed(42)
+
+n_records, n_features = features.shape
+last_loss = None
+
+# Initialize weights
+weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+
+# Neural Network hyperparameters
+epochs = 1000
+learnrate = 0.5
+
+for e in range(epochs):
+    del_w = np.zeros(weights.shape)
+    for x, y in zip(features.values, targets):
+        # Loop through all records, x is the input, y is the target
+
+        h = np.dot(x, weights)
+
+        # TODO: Calculate the output
+        output = sigmoid(h)
+
+        # TODO: Calculate the error
+        error = y - sigmoid(np.dot(x, weights))
+
+        # TODO: Calculate change in weights
+        del_w += learnrate * (error*sigmoid_prime(h)) * x
+
+    # TODO: Update weights
+    m = len(features.values)
+    weights += 1/m * del_w
+
+    # Printing out the mean square error on the training set
+    if e % (epochs / 10) == 0:
+        out = sigmoid(np.dot(features, weights))
+        loss = np.mean((out - targets) ** 2)
+        if last_loss and last_loss < loss:
+            print("Train loss: ", loss, "  WARNING - Loss Increasing")
+        else:
+            print("Train loss: ", loss)
+        last_loss = loss
+
+
+# Calculate accuracy on test data
+tes_out = sigmoid(np.dot(features_test, weights))
+predictions = tes_out > 0.5
+accuracy = np.mean(predictions == targets_test)
+print("Prediction accuracy: {:.3f}".format(accuracy))
