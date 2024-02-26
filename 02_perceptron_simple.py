33fe2e3227e58c26ddb19a2ec9fa0284dd0a59a2
Irving Vasquez <jirvingvg@gmail.com>
2024-02-26 15:32:13 -0600

    add perceptron simple

---
+++ 02_perceptron_simple.py
@@ -0,0 +1,31 @@
+# jivg.org
+
+def combinacion_lineal (X , W , b):
+    suma = 0
+    for w , x in zip (W , X ):
+        suma = suma + w * x
+    suma = suma + b
+    return suma
+
+def escalon ( h ):
+    if h >= 0 :
+        return 1
+    else :
+        return 0
+    
+
+import numpy as np
+
+def perceptron(W, X, b, activacion):
+    h = combinacion_lineal(W, X, b)
+    return activacion(h)
+
+inputs = np.array([0.7 , -0.3 ])
+weights = np.array([0.1 , 0.8 ])
+bias = 0.5
+
+# Pase frontal
+activacion = escalon
+output = perceptron(weights, inputs, bias, activacion)
+print ( ' Output : ')
+print ( output )
