5e60a9f2d1d3067158329720d5e67fe3fe9a6421
Irving Vasquez <jirvingvg@gmail.com>
2024-03-12 10:08:39 -0600

    update exercise 04

---
+++ soluciones/04_red_neuronal_simple_solucion.py
@@ -0,0 +1,33 @@
+# jivg.org
+import numpy as np
+
+def combinacion_lineal (X , W , b):
+    h = np.dot(W,X) + b
+    return h
+
+def sigmoide(h):
+    result = 1/(1+np.exp(-h))
+    return result
+
+def neurona(W, X, b, activacion):
+    h = combinacion_lineal(W, X, b)
+    return activacion(h)
+
+def main():
+    print("Red neuronal simple")
+
+    entradas = np.array([1, 1])
+    print("X: ", entradas)
+    pesos = np.array([.2, .2])
+    print("W: ", pesos)
+    sesgo = 1
+    print("b: ", sesgo)
+    
+    # inferencia o pase frontal
+    activacion = sigmoide
+    output = neurona(pesos, entradas, sesgo, activacion)
+
+    print("Resultado: ", output)
+
+if __name__ == "__main__":
+    main()
